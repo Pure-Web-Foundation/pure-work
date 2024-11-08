3096127c0595453d6f7ad7a873f59dd7b3ac426e
Marc van Neerven <cto@qogni.com>
2024-11-08 08:33:17 +0100

    Cleanup

--- src/js/broker/index.js
+++ src/js/broker/index.js
@@ -1,7 +1,7 @@
 import { EventTargetMixin } from "../common";
 
 /**
- * Broker class. Acts as a pub-sub broker for app-wide messaging.
+ * Singleton Broker class. Acts as a pub-sub broker for app-wide messaging.
  */
 export class Broker extends EventTargetMixin(EventTarget) {
   static #instance;
@@ -11,8 +11,6 @@ export class Broker extends EventTargetMixin(EventTarget) {
   constructor() {
     super();
     if (Broker.#instance) return Broker.#instance; // singleton
-
-    console.log("Creating singleton Broker...");
     Broker.#instance = this;
   }
 
@@ -20,7 +18,7 @@ export class Broker extends EventTargetMixin(EventTarget) {
    * Gets the singleton instance of the broker class.
    */
   static get instance() {
-    if (!this.#instance) new Broker();
+    if (!this.#instance) new this();
     return this.#instance;
   }
 
@@ -42,25 +40,4 @@ export class Broker extends EventTargetMixin(EventTarget) {
     );
     await Promise.all(promises);
   }
-
-  // /**
-  //  * Publishes a message on the broker.
-  //  * @param {String} topicName
-  //  * @param {Object} details
-  //  */
-  // publish(topicName, details) {
-  //   return this.fire(`pub-${topicName}`, details);
-  // }
-
-  // /**
-  //  * Subscribes to a topic on the broker.
-  //  * @param {String} topicName
-  //  * @param {Function} callback
-  //  * @returns {Broker} instance (for easy chaining)
-  //  */
-  // subscribe(topicName, callback) {
-  //   return this.on(`pub-${topicName}`, async (e) => {
-  //     await callback(e.detail);
-  //   });
-  // }
 }
